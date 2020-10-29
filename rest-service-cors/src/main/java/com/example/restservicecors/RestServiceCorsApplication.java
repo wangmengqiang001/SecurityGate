e6c6a8cc732f424043aab180968587132134c765
wangmengqiang <wangmengqiang001@126.com>
2020-10-29 17:37:35 +0800

    close #7, this is real
    Signed-off-by: wangmengqiang <wangmengqiang001@126.com>

---
+++ rest-service-cors/src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservicecors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RestServiceCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceCorsApplication.class, args);
+	}
+}
