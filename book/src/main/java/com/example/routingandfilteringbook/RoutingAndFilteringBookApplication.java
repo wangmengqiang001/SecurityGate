faf73bb44d6820633bbcb4ecb71eac679abbbff6
wangmengqiang <wangmengqiang001@126.com>
2020-10-29 11:20:08 +0800

    close #8, service following gateway
    Signed-off-by: wangmengqiang <wangmengqiang001@126.com>

---
+++ book/src/main/java/com/example/routingandfilteringbook/RoutingAndFilteringBookApplication.java
@@ -0,0 +1,27 @@
+package com.example.routingandfilteringbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@RestController
+@SpringBootApplication
+@EnableEurekaClient
+public class RoutingAndFilteringBookApplication {
+
+  @RequestMapping(value = "/available")
+  public String available() {
+    return "Spring in Action";
+  }
+
+  @RequestMapping(value = "/checked-out")
+  public String checkedOut() {
+    return "Spring Boot in Action";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(RoutingAndFilteringBookApplication.class, args);
+  }
+}
